04384d389457346470266c060af4c447cd3c16e3
Jason Yegge <Lansatac@gmail.com>
2023-09-30 16:20:27 -0600

    Adding basic Vibe.d project skeleton

---
+++ source/app.d
@@ -0,0 +1,20 @@
+import std.stdio;
+import vibe.vibe;
+
+void index(HTTPServerRequest req, HTTPServerResponse res)
+{
+	res.render!("index.dt", req);
+}
+
+void main()
+{
+	auto router = new URLRouter;
+	router.get("/", &index);
+	
+	auto settings = new HTTPServerSettings;
+	settings.port = 8080;
+	
+	listenHTTP(settings, router);
+	
+	runApplication();
+}
